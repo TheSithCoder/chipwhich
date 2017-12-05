a5bac9071b629c7189423cc7770a3a25dc2f095e
articDrag0n <picubed314@gmail.com>
2017-12-05 13:12:17 -0500

    chipwhich

---
+++ code.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE chipwhich;
+USE chipwhich;
+CREATE TABLE transactionRecords(
+transactionNumber int not null auto_increment,
+name varchar(255),
+startingPrice float,
+finalPrice float,
+PRIMARY KEY (transactionNumber));
+INSERT INTO transactionRecords VALUES (null, "Liam BOI", 150, 180);
+SELECT * FROM transactionRecords;
+SELECT * FROM transactionRecords WHERE transactionNumber = 1;
